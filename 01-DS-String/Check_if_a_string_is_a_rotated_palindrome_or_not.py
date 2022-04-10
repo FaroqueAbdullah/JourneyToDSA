338dcf23ad3fcaae1acd4d6325826da5108189cd
Faroque Abdullah <abdullahabir3@gmail.com>
2022-04-10 16:07:07 +0600

    is rotate palindrome

--- 01-DS-String/Check_if_a_string_is_a_rotated_palindrome_or_not.py
+++ 01-DS-String/Check_if_a_string_is_a_rotated_palindrome_or_not.py
@@ -1,7 +1,21 @@
+def isPalindrome(string, low, high):
 
+  return low >= high or ( string[low] == string[high] and isPalindrome(string, low + 1, high - 1) )
+
+def isRotatePalindrome(string):
+  n = len(string)
+
+  for i in range(n):
+    string = string[1:] + string[0]
+
+    if isPalindrome(string, 0, n - 1):
+      return True
+
+  return False
 
 
 if __name__ == '__main__':
 
-  string = "ABCDCBA"
-  print()
+  string = "CBAABCD".replace(" ", "")
+  
+  print(isRotatePalindrome(string))
