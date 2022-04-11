aa08de8e35bd999ee8d964e0d2d51ba6413982f0
faroque abdullah <abdullahabir3@gmail.com>
2022-04-11 21:53:17 +0600

    problem solved

--- 01-DS-String/Longest Palindrome.js
+++ 01-DS-String/Longest Palindrome.js
@@ -1,3 +1,4 @@
+// find leargest palindrome length
 var longestPalindrome = function(s) {
     var savedData = {}
     var longestPalindrome = 0
@@ -25,4 +26,47 @@ var longestPalindrome = function(s) {
     }
     
     return longestPalindrome + singlenumber
+};
+
+
+
+
+// find leargeest palindrome
+var longgest = ''
+
+var isPalindromeCheck = function( str, low, high ) {
+    return low >= high || ((str[low] == str[high]) && isPalindromeCheck(str, low + 1, high - 1) )
+}
+
+var isPalindrome = function( str ) {
+    low = 0
+    high = str.length - 1
+    return isPalindromeCheck( str, low, high )
+}
+
+var checklength = function(str) {
+    if ( longgest.length < str.length ) {
+        longgest = str
+    }
+}
+
+
+var longestPalindrome = function(s) {
+    
+    for (var i = 0; i < s.length; i++) {
+        for (var j = 1; j <= s.length; j++) {
+            if ( isPalindrome( s.slice(i,j) )) {
+                checklength(s.slice(i,j))
+            }
+        }
+        
+        for (var j = s.length; j > 0; j--) {
+             if ( isPalindrome( s.slice(i,j)) ) {
+                checklength(s.slice(i,j))
+            }
+        }
+    }
+    
+    return longgest
+    // console.log(isPalindrome('AAA', 0, 2))
 };
